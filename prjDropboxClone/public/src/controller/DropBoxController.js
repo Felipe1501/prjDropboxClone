c85e815c23926fde69cf2a73172a94d822b8ded3
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-12 09:05:18 -0300

    aula 121 fix: browsing folders or opening files

--- prjDropboxClone/public/src/controller/DropBoxController.js
+++ prjDropboxClone/public/src/controller/DropBoxController.js
@@ -6,6 +6,7 @@ class DropBoxController {
 
         this.onselectionchange = new Event('selectionchange');
 
+        this.navEl = document.querySelector('#browse-location');
         this.btnSendFileEl = document.querySelector("#btn-send-file")
         this.inputFilesEl = document.querySelector('#files');
         this.snackModalEl = document.querySelector('#react-snackbar-root');
@@ -21,7 +22,7 @@ class DropBoxController {
 
         this.connectFirebase();
         this.initEvents();
-        this.readFiles();
+        this.openFolder();
         
 
 
@@ -185,9 +186,11 @@ class DropBoxController {
         this.btnSendFileEl.disabled = false;
     }
 
-    getFirebaseRef(){
+    getFirebaseRef(path){
 
-        return firebase.database().ref('files')
+        if(!path) path = this.currentFolder.join('/');
+
+        return firebase.database().ref(path)
 
     }
 
@@ -483,6 +486,8 @@ class DropBoxController {
     //função para ler os arquivos no firebase
     readFiles(){
 
+        this.lastFolder = this.currentFolder.join('/');
+
         this.getFirebaseRef().on('value', snapshot => {
             
             //vai limpar para colocar um novo html
@@ -493,16 +498,99 @@ class DropBoxController {
                 let key = snapshotItem.key;
                 //propriedade
                 let data = snapshotItem.val();
-
-
+        
+                if (data.type){
                 this.listFilesEl.appendChild(this.getFileView(data, key));
+                }
             });
         });
 
     }
 
+    openFolder() {
+
+        if (this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value');
+
+        this.renderNav();
+        this.readFiles();
+
+    }
+
+    renderNav(){
+
+
+        let nav = document.createElement('nav');
+        //variável que vai salvar o caminho para as pastas
+        let path = [];
+
+        for (let i = 0; i < this.currentFolder.length; i++){
+
+            let folderName = this.currentFolder[i];
+            let span = document.createElement('span');
+
+            path.push(folderName);
+
+            //conteúdo da span só vai receber o nome da pasta
+            if( (i + 1) === this.currentFolder.length){
+
+                span.innerHTML = folderName;
+
+            }else{
+                span.className = 'breadcrumb-segment__wrapper';
+                span.innerHTML = `
+                <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                                                <a href="#" data-path="${path.join('/')}" class="breadcrumb-segment">${folderName}</a>
+                                            </span>
+                                            <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                                                <title>arrow-right</title>
+                                                <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+                                            </svg>
+                `;
+
+                
+            }
+            nav.appendChild(span);
+        }
+        //copiando conteúdo do nav que está na memória, para o conteúdo do nav na tela
+        this.navEl.innerHTML = nav.innerHTML;
+
+        this.navEl.querySelectorAll('a').forEach(a =>{
+            a.addEventListener('click', e=> {
+                e.preventDefault();
+
+                this.currentFolder = a.dataset.path.split('/');
+
+                this.openFolder();
+            });
+        });
+        /*
+        <span class="breadcrumb-segment__wrapper">
+                                            
+                                        </span>
+         */
+
+    }
+
     //função que irá mostrar o arquivo que foi selecionado
     initEventsLi(li){
+
+        li.addEventListener('dblclick', e=> {
+
+            let file = JSON.parse(li.dataset.file);
+
+            switch (file.type){
+
+                case 'folder':
+                    this.currentFolder.push(file.name);
+                    this.openFolder();
+                break;
+
+                default:
+                    window.open('/file?path=' + file.path);
+
+            }
+
+        });
 
         li.addEventListener('click', e => {
                
